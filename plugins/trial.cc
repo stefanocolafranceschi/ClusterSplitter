ff5e1de1d7bbac1eb9d00c13ad4292a0b44f3651
tetto <tetto@cern.ch>
2024-11-16 03:35:14 +0100

    test version (broken)

--- plugins/trial.cc
+++ plugins/trial.cc
@@ -14,181 +14,109 @@
 // Original Author:  Stefano Colafranceschi
 //         Created:  Mon, 11 Nov 2024 22:44:44 GMT
 //
-//
 
-// system include files
-#include <memory>
+// Alpaka headers
+#include <alpaka/alpaka.hpp>
+#include <alpaka/acc/AccGpuCudaRt.hpp>  // Add this for CUDA support
+
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/workdivision.h"
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/stream/EDProducer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/StreamID.h"
 
 // system include files
 #include "TFile.h"
 #include "TString.h"
-
-// Alpaka headers
-#include <alpaka/alpaka.hpp>
-#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
-#include "HeterogeneousCore/AlpakaInterface/interface/workdivision.h"
-
+#include <memory>
+#include <iostream>
 
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
+    using Acc = acc::AccGpuCudaRt<dim::Dim1, uint32_t>;  // Dim1 refers to a 1D grid, uint32_t for indices
+    using DevAcc = dev::Dev<Acc>;  // Define the device based on the accelerator type
+    using Queue = queue::QueueCudaRtSync;  // For GPU queues
 
-using namespace cms::alpakatools;
+    // Define a simple Alpaka kernel as a struct
+    struct DummyKernel {
+        template <typename Acc, typename T>
+        ALPAKA_FN_ACC void operator()(Acc const& acc, T* result) const {
+            // Simple computation for each thread
+            auto idx = alpaka::idx::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0];
+            result[idx] = idx * 2;  // Example dummy computation
+        }
+    };
 
-// Define the Alpaka accelerator type
-using Acc = alpaka::acc::AccGpuCudaRt<alpaka::dim::Dim1, uint32_t>;
-using DevAcc = alpaka::dev::Dev<Acc>;
-using Queue = alpaka::queue::QueueCudaRtSync;  // For GPU
+    class trial : public edm::stream::EDProducer<> {
+    public:
+        explicit trial(const edm::ParameterSet&);
+        ~trial() override;
 
-// Define a simple Alpaka kernel as a struct
-struct DummyKernel {
-  template <typename Acc, typename T>
-  ALPAKA_FN_ACC void operator()(Acc const& acc, T* result) const {
-    // Simple computation for each thread
-    auto idx = alpaka::idx::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0];
-    result[idx] = idx * 2;  // Example dummy computation
-  }
-};
+        static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
-//
-// class declaration
-//
+    private:
+        void beginStream(edm::StreamID) override;
+        void produce(edm::Event&, const edm::EventSetup&) override;
+        void endStream() override;
 
-class trial : public edm::stream::EDProducer<> {
-public:
-  explicit trial(const edm::ParameterSet&);
-  ~trial() override;
+        // member data
+        std::string configString_;  // Holds the configuration string passed from Python
+        TFile* rootFile_;
+    };
 
-  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+    // Constructor
+    trial::trial(const edm::ParameterSet& iConfig)
+        : configString_(iConfig.getParameter<std::string>("configString")) {
+        // Initialize ROOT file and write configuration string
+        rootFile_ = new TFile("config_output.root", "RECREATE");
+    }
 
-private:
-  void beginStream(edm::StreamID) override;
-  void produce(edm::Event&, const edm::EventSetup&) override;
-  void endStream() override;
+    // Destructor
+    trial::~trial() {
+        if (rootFile_) {
+            rootFile_->Close();
+            delete rootFile_;
+        }
+    }
 
-  //void beginRun(edm::Run const&, edm::EventSetup const&) override;
-  //void endRun(edm::Run const&, edm::EventSetup const&) override;
-  //void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-  //void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+    // produce method (updated version)
+    void trial::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
+        // Define a device for the accelerator
+        DevAcc devAcc = alpaka::pltf::getDevByIdx<DevAcc>(0);  // Retrieve device
+        alpaka::queue::QueueCudaRtSync queue(devAcc);  // Set up the queue on this device
 
-  // ----------member data ---------------------------
-std::string configString_;  // Holds the configuration string passed from Python
-  TFile* rootFile_;
+        // Define work divisions (e.g., grid and block sizes)
+        const uint32_t elements = 1024;  // Number of elements to process
+        auto workDiv = alpaka::workdiv::WorkDivMembers<1>(elements, 1, 1);
 
-};
+        // Use simple uint32_t buffer for memory management
+        auto hostBuf = alpaka::allocBuf<uint32_t>(alpaka::pltf::getDevByIdx<alpaka::pltf::PlatformCpu>(0), elements);
+        auto devBuf = alpaka::allocBuf<uint32_t>(devAcc, elements);
 
-//
-// constants, enums and typedefs
-//
+        // Launch the kernel (DummyKernel is assumed to be defined earlier)
+        DummyKernel kernel;
+        alpaka::exec<Acc>(queue, workDiv, kernel, devBuf.data());
 
-//
-// static data member definitions
-//
+        // Copy the results back to host memory
+        alpaka::mem::view::copy(queue, hostBuf, devBuf, elements);
+        alpaka::wait(queue);
 
-//
-// constructors and destructor
-//
-trial::trial(const edm::ParameterSet& iConfig)
-    : configString_(iConfig.getParameter<std::string>("configString")) {
-  // Initialize ROOT file and write configuration string
-  rootFile_ = new TFile("config_output.root", "RECREATE");
+        // For debugging, output the result
+        for (size_t i = 0; i < elements; ++i) {
+            std::cout << "Result[" << i << "] = " << hostBuf[i] << std::endl;
+        }
+    }
+
+    // Fill descriptions for the configuration
+    void trial::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+        edm::ParameterSetDescription desc;
+        desc.add<std::string>("configString", "default")->setComment("Configuration string to store in ROOT file");
+        descriptions.add("trial", desc);
+    }
 }
-
-
-trial::~trial() {
-  if (rootFile_) {
-    rootFile_->Close();
-    delete rootFile_;
-  }
-}
-//
-// member functions
-//
-
-void trial::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-    // Define a device for the accelerator
-    DevAcc devAcc = alpaka::pltf::getDevByIdx<DevAcc>(0);  // Retrieve device
-    Queue queue(devAcc);  // Set up the queue on this device
-
-    // Define work divisions (e.g., grid and block sizes)
-    const uint32_t elements = 1024;  // Number of elements to process
-    auto workDiv = alpaka::workdiv::WorkDivMembers<1>(elements, 1, 1);
-
-    // Allocate host and device memory
-    auto hostBuf = alpaka::allocBuf<uint32_t, uint32_t>(alpaka::pltf::getDevByIdx<alpaka::pltf::PlatformCpu>(0), elements);
-    auto devBuf = alpaka::allocBuf<uint32_t, uint32_t>(devAcc, elements);
-
-    // Set up the queue for async execution
-    alpaka::queue::QueueCudaRtSync queue(devAcc);
-
-    // Launch the kernel
-    DummyKernel kernel;
-    alpaka::exec<Acc>(queue, workDiv, kernel, devBuf.data());
-
-    // Copy the results back to host memory
-    alpaka::mem::view::copy(queue, hostBuf, devBuf, elements);
-    alpaka::wait(queue);
-
-    // Here, you can store results in a ROOT file or use them as needed
-}
-
-
-// ------------ method called once each stream before processing any runs, lumis or events  ------------
-void trial::beginStream(edm::StreamID) {
-  // please remove this method if not needed
-}
-
-// ------------ method called once each stream after processing all runs, lumis and events  ------------
-void trial::endStream() {
-  // please remove this method if not needed
-}
-
-// ------------ method called when starting to processes a run  ------------
-/*
-void
-trial::beginRun(edm::Run const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when ending the processing of a run  ------------
-/*
-void
-trial::endRun(edm::Run const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when starting to processes a luminosity block  ------------
-/*
-void
-trial::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
-}
-*/
-
-// ------------ method called when ending the processing of a luminosity block  ------------
-/*
-void
-trial::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
-{
-}
-*/
-
-void trial::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  edm::ParameterSetDescription desc;
-  desc.add<std::string>("configString", "default")->setComment("Configuration string to store in ROOT file");
-  descriptions.add("trial", desc);
-}
-
-//define this as a plug-in
-}
-DEFINE_FWK_MODULE(trial);
+// Define this as a plug-in
+DEFINE_FWK_MODULE(ALPAKA_ACCELERATOR_NAMESPACE::trial);
